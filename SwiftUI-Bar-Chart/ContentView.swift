b937cb09e6fdcbc84879f2f4305c313384c999cb
Bohdan Dovhal <dovhalwork@gmail.com>
2025-01-19 14:45:06 +0200

    Added Animations

--- SwiftUI-Bar-Chart/ContentView.swift
+++ SwiftUI-Bar-Chart/ContentView.swift
@@ -10,26 +10,21 @@ import Charts
 
 struct ContentView: View {
     
-    let viewMonths: [ViewMonth] = [
-        .init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 55000),
-        .init(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 89000),
-        .init(date: Date.from(year: 2023, month: 3, day: 1), viewCount: 64000),
-        .init(date: Date.from(year: 2023, month: 4, day: 1), viewCount: 79000),
-        .init(date: Date.from(year: 2023, month: 5, day: 1), viewCount: 130000),
-        .init(date: Date.from(year: 2023, month: 6, day: 1), viewCount: 90000),
-        .init(date: Date.from(year: 2023, month: 7, day: 1), viewCount: 88000),
-        .init(date: Date.from(year: 2023, month: 8, day: 1), viewCount: 64000),
-        .init(date: Date.from(year: 2023, month: 9, day: 1), viewCount: 74000),
-        .init(date: Date.from(year: 2023, month: 10, day: 1), viewCount: 99000),
-        .init(date: Date.from(year: 2023, month: 11, day: 1), viewCount: 110000),
-        .init(date: Date.from(year: 2023, month: 12, day: 1), viewCount: 94000)
-    ]
+    @State private var rawSelectedDate: Date?
+    
+    var selectedViewMonth: ViewMonth? {
+        guard let rawSelectedDate else { return nil }
+        return ViewMonth.mockData.first {
+            Calendar.current.isDate(rawSelectedDate, equalTo: $0.date, toGranularity: .month)
+        }
+    }
+    
     
     var body: some View {
         VStack( alignment: .leading, spacing: 4) {
             Text("YouTube Views")
             
-            Text("Total: \(viewMonths.reduce(0, { $0 + $1.viewCount}))")
+            Text("Total: \(ViewMonth.mockData.reduce(0, { $0 + $1.viewCount}))")
                 .fontWeight(.semibold)
                 .font(.footnote)
                 .foregroundStyle(.secondary)
@@ -44,15 +39,33 @@ struct ContentView: View {
 //                            .font(.caption)
 //                            .foregroundStyle(.secondary)
 //                    }
-                
-                ForEach(viewMonths) { viewMonth in
+                if let selectedViewMonth {
+                    RuleMark(x: .value("Selected Month", selectedViewMonth.date, unit: .month))
+                        .foregroundStyle(.secondary.opacity(0.3))
+                        .annotation(position: .top, overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) {
+                            VStack {
+                                Text(selectedViewMonth.date, format: .dateTime.month(.wide))
+                                    .bold()
+                                Text("\(selectedViewMonth.viewCount)")
+                                    .font(.title3.bold())
+                            }
+                            .foregroundStyle(.white)
+                            .padding(12)
+                            .frame(width: 120)
+                            .background(RoundedRectangle(cornerRadius: 10).fill(.pink.gradient))
+                        }
+                }
+                    
+                ForEach(ViewMonth.mockData) { viewMonth in
                     BarMark(x: .value("Month", viewMonth.date, unit: .month),
                             y: .value("Views", viewMonth.viewCount)
                     )
                     .foregroundStyle(.pink)
+                    .opacity(rawSelectedDate == nil || viewMonth.date == selectedViewMonth?.date ? 1.0 : 0.3)
                 }
             }
             .frame(height: 180)
+            .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
             .chartXAxis {
                 AxisMarks(values: .stride(by: .month, count: 1)) {
                     AxisValueLabel(format: .dateTime.month(.narrow), centered: true)
@@ -97,6 +110,21 @@ struct ViewMonth: Identifiable {
     let id = UUID()
     let date: Date
     let viewCount: Int
+    
+    static let mockData: [ViewMonth] = [
+        .init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 55000),
+        .init(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 89000),
+        .init(date: Date.from(year: 2023, month: 3, day: 1), viewCount: 64000),
+        .init(date: Date.from(year: 2023, month: 4, day: 1), viewCount: 79000),
+        .init(date: Date.from(year: 2023, month: 5, day: 1), viewCount: 130000),
+        .init(date: Date.from(year: 2023, month: 6, day: 1), viewCount: 90000),
+        .init(date: Date.from(year: 2023, month: 7, day: 1), viewCount: 88000),
+        .init(date: Date.from(year: 2023, month: 8, day: 1), viewCount: 64000),
+        .init(date: Date.from(year: 2023, month: 9, day: 1), viewCount: 74000),
+        .init(date: Date.from(year: 2023, month: 10, day: 1), viewCount: 99000),
+        .init(date: Date.from(year: 2023, month: 11, day: 1), viewCount: 110000),
+        .init(date: Date.from(year: 2023, month: 12, day: 1), viewCount: 94000)
+    ]
 }
 
 extension Date {
