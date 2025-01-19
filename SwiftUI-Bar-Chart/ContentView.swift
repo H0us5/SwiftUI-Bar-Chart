fb4e5a85385ada21535b107c88a26fd83b39cbe3
Bohdan Dovhal <dovhalwork@gmail.com>
2025-01-19 11:50:13 +0200

    Initial Commit

---
+++ SwiftUI-Bar-Chart/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  SwiftUI-Bar-Chart
+//
+//  Created by Bohdan Dovhal on 19.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
+struct ViewMonth: Identifiable {
+    let id = UUID()
+    let date: Date
+    let viewCount: Int
+}
+
+extension Date {
+    static func from(year: Int, month: Int, day: Int) -> Date {
+        let components = DateComponents(year: year, month: month, day: day)
+        return Calendar.current.date(from: components)!
+    }
+}
---
+++ SwiftUI-Bar-Chart/SwiftUI_Bar_ChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Bar_ChartApp.swift
+//  SwiftUI-Bar-Chart
+//
+//  Created by Bohdan Dovhal on 19.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Bar_ChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
