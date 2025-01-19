1d85ac0e2fb2f027878098cc29fe2f08f1717fe5
Bohdan Dovhal <dovhalwork@gmail.com>
2025-01-19 11:57:07 +0200

    Project Setup

--- SwiftUI-Bar-Chart/ContentView.swift
+++ SwiftUI-Bar-Chart/ContentView.swift
@@ -8,6 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let viewMonth: [ViewMonth] = [
+        .init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 55000),
+        .init(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 89000),
+        .init(date: Date.from(year: 2023, month: 3, day: 1), viewCount: 64000),
+        .init(date: Date.from(year: 2023, month: 4, day: 1), viewCount: 79000),
+        .init(date: Date.from(year: 2023, month: 5, day: 1), viewCount: 130000),
+        .init(date: Date.from(year: 2023, month: 6, day: 1), viewCount: 90000),
+        .init(date: Date.from(year: 2023, month: 7, day: 1), viewCount: 88000),
+        .init(date: Date.from(year: 2023, month: 8, day: 1), viewCount: 64000),
+        .init(date: Date.from(year: 2023, month: 9, day: 1), viewCount: 74000),
+        .init(date: Date.from(year: 2023, month: 10, day: 1), viewCount: 99000),
+        .init(date: Date.from(year: 2023, month: 11, day: 1), viewCount: 110000),
+        .init(date: Date.from(year: 2023, month: 12, day: 1), viewCount: 94000)
+    ]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
