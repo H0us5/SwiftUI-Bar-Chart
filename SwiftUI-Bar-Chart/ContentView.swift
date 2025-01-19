aa5fc4b31c28b690058a09ea0b33c3ec68e76997
Bohdan Dovhal <dovhalwork@gmail.com>
2025-01-19 12:25:36 +0200

    Finish Chart without Animation

--- SwiftUI-Bar-Chart/ContentView.swift
+++ SwiftUI-Bar-Chart/ContentView.swift
@@ -6,10 +6,11 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
     
-    let viewMonth: [ViewMonth] = [
+    let viewMonths: [ViewMonth] = [
         .init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 55000),
         .init(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 89000),
         .init(date: Date.from(year: 2023, month: 3, day: 1), viewCount: 64000),
@@ -25,11 +26,63 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack( alignment: .leading, spacing: 4) {
+            Text("YouTube Views")
+            
+            Text("Total: \(viewMonths.reduce(0, { $0 + $1.viewCount}))")
+                .fontWeight(.semibold)
+                .font(.footnote)
+                .foregroundStyle(.secondary)
+                .padding(.bottom, 12)
+            
+            Chart {
+                RuleMark(y: .value("Goal", 80000))
+                    .foregroundStyle(.mint)
+                    .lineStyle(StrokeStyle(lineWidth: 1, dash: [5]))
+//                    .annotation(alignment: .leading) {
+//                        Text("Goal")
+//                            .font(.caption)
+//                            .foregroundStyle(.secondary)
+//                    }
+                
+                ForEach(viewMonths) { viewMonth in
+                    BarMark(x: .value("Month", viewMonth.date, unit: .month),
+                            y: .value("Views", viewMonth.viewCount)
+                    )
+                    .foregroundStyle(.pink)
+                }
+            }
+            .frame(height: 180)
+            .chartXAxis {
+                AxisMarks(values: .stride(by: .month, count: 1)) {
+                    AxisValueLabel(format: .dateTime.month(.narrow), centered: true)
+                }
+            }
+            .chartYAxis {
+                AxisMarks { mark in
+                    AxisValueLabel()
+                    AxisGridLine()
+                }
+            }
+            .padding(.bottom)
+//            .chartYScale(domain: 0...200000)
+//            .chartXAxis(.hidden)
+//            .chartYAxis(.hidden)
+//            .chartPlotStyle {plotContent in
+//                plotContent
+//                    .background(.black.gradient.opacity(0.3))
+//                    .border(.green, width: 3)
+//            }
+            HStack {
+                Image(systemName: "line.diagonal")
+                    .rotationEffect(Angle(degrees: 45))
+                    .foregroundStyle(.mint)
+            
+                Text("Monthly Goal")
+                    .foregroundStyle(.secondary)
+            }
+            .font(.caption2)
+            .padding(.leading, 4)
         }
         .padding()
     }
